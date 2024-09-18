1526a132e3db9261146929c85a9a3e677fcbc455
[Cody Williams] <[me@codythedoer.com]>
2024-09-17 20:21:43 -0400

    Dropped file selection while in CLI

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,57 @@
 use std::io;
 
 fn main() {
-    let user_entry = inital_user_selection_data_type();
+    println!("Welcome to the Mean and Median Idenitifier!");
+
     let mut storage_vec = Vec::new();
+    data_entry_type_selection(&mut storage_vec);
+    main_again();
+}
+
+fn data_entry_type_selection(v: &mut Vec<String>) {
+    let user_entry = inital_user_selection_data_type();
 
     if user_entry == "M" {
-        manual_entry(&mut storage_vec);
-        
-    } else if user_entry == "F" {
-        println!("F");
-    } else {
-        println!("{user_entry}");
+        manual_entry(v);
+    } else { // Direct vector entry for first input and then run manual input loop
+        let mut clean_float = Vec::new();
+        for char in user_entry.chars() {
+            if char.is_ascii_digit() || char == '.'{
+                clean_float.push(char as u8)
+            }
+        }
+        v.push(String::from_utf8(clean_float).expect("Our bytes should be valid utf8"));
+        manual_entry(v);
     }
 
     println!("Vector Review:");
-    for i in storage_vec.iter() { 
+    for i in v.iter() { 
         println!("{}", i); 
     }
+}
 
-    main_again();
+fn inital_user_selection_data_type() -> String {
+    println!("Please let me know, are you entering integers manually 'M' or using a pregenerated file 'F'? (M/F)");
+
+    let mut data_entry_type_manual_or_file = String::new();
+
+    io::stdin()
+        .read_line(&mut data_entry_type_manual_or_file)
+        .expect("Failed to read line");
+
+    let trimmed_input = data_entry_type_manual_or_file.trim();
+
+    match trimmed_input {
+        "m" | "M" => {"M".to_string()},
+        _=> {
+            match trimmed_input.parse::<f32>() {
+                Ok(value) => {value.to_string()},
+                Err(_) => {
+                    "Invalid Input".to_string()
+                },
+            }
+        },
+    }
 }
 
 fn manual_entry(v: &mut Vec<String>) {
@@ -47,39 +80,10 @@ fn manual_entry(v: &mut Vec<String>) {
             if char == 'x' || char == 'X'{
                 loop_check = false;
                 println!("Exiting manual entry");
-            }    
+            }
         }
     }
 }
-
-// fn file_selection() {}
-
-fn inital_user_selection_data_type() -> String {
-    println!("Welcome to the Mean and Median Idenitifier!");
-    println!("Please let me know, are you entering integers manually 'M' or using a pregenerated file 'F'? (M/F)");
-
-    let mut data_entry_type_manual_or_file = String::new();
-
-    io::stdin()
-        .read_line(&mut data_entry_type_manual_or_file)
-        .expect("Failed to read line");
-
-    let trimmed_input = data_entry_type_manual_or_file.trim();
-
-    match trimmed_input {
-        "m" | "M" => {"M".to_string()},
-        "f" | "F" => {"F".to_string()},
-        _=> {
-            match trimmed_input.parse::<f32>() {
-                Ok(value) => {value.to_string()},
-                Err(_) => {
-                    "Invalid Input".to_string()
-                },
-            }
-        },
-    }
-}
-
 
 fn main_again() {
     println!("Would you like to sequence a new Mean and Median? (Y/N)");
@@ -94,12 +98,11 @@ fn main_again() {
             main()
         },
         "n" | "N" => {
-            println!("Exiting...");
-            return;
+            println!("Exiting...")
         },
         _ => {
-            println!("Failure: Bad Input, exiting...");
-            return;
+            println!("Failure: Bad Input, exiting...")
         },
     };
 }
+
